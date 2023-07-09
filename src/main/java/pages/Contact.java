414521ed38f9f5b35de2b67998134f40efb4388a
si0107 <sarikaislam.02@gmail.com>
2023-07-09 16:37:11 -0400

    Created browser class and contacts page

--- src/main/java/pages/Contact.java
+++ src/main/java/pages/Contact.java
@@ -1,4 +1,14 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Contact {
+
+    @FindBy(name = "first_name") WebElement firstName;
+    @FindBy(name = "last_name") WebElement lastName;
+    @FindBy(name = "email") WebElement email;
+    @FindBy(name = "message") WebElement comment;
+    @FindBy(css = "[value=\"SUBMIT\"]") WebElement submitButton;
+
 }
