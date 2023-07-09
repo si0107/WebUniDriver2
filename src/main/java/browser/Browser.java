67b51497d5b84543f6373db38ab6123d56fe9b95
sofianebouchik <sosobouchik@gmail.com>
2023-07-09 16:39:48 -0400

    hi

--- src/main/java/browser/Browser.java
+++ src/main/java/browser/Browser.java
@@ -1,5 +1,27 @@
 package browser;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Browser {
+    private static String webDriverName= "chrome";
+   static WebDriver driver;
+    public void setDriver(){
+        if(webDriverName.equalsIgnoreCase("chrome"))
+            driver=new ChromeDriver();
+        else if (webDriverName.equalsIgnoreCase("firfox")) {
+            driver=new FirefoxDriver();
+
+
+        }
+        driver=new EdgeDriver();
+
+    }
+    public static WebDriver getDriver(){
+        return driver;
+    }
+
     
 }
--- src/main/java/pages/Contact.java
+++ src/main/java/pages/Contact.java
@@ -1,4 +1,13 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Contact {
+    @FindBy(name = "first_name")private WebElement firstName;
+    @FindBy(name = "last_name")private WebElement lastName;
+    @FindBy(name = "email")private WebElement email;
+    @FindBy(name ="message")private WebElement Message;
+    @FindBy(css = "[value=\"SUBMIT\"]")private WebElement submit;
 }
