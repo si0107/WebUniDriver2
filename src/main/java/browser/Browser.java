9ae719409d0e06bc25e46677cbbc6bd39d9d5d7a
kazi8 <kazifakrul88@gmail.com>
2023-07-09 16:38:27 -0600

    added content to browser and contact page

--- src/main/java/browser/Browser.java
+++ src/main/java/browser/Browser.java
@@ -1,5 +1,25 @@
 package browser;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Browser {
-    
+    static WebDriver driver;
+    private static String webdriverName="Chorme";
+
+    public static void  setWebDriver() {
+        if (webdriverName.equalsIgnoreCase("Chorme")) {
+            driver = new ChromeDriver();
+        } else if (webdriverName.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        } else {
+            driver = new EdgeDriver();
+        }
+
+    }
+    public static WebDriver getWebDriver(){
+        return driver;
+    }
 }
