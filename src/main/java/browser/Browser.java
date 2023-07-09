5d479cd07225782d4b27b85d711aee86cc667c15
Saif <saif@Saifs-MacBook-Pro.local>
2023-07-09 16:42:17 -0400

    message

--- src/main/java/browser/Browser.java
+++ src/main/java/browser/Browser.java
@@ -1,5 +1,27 @@
 package browser;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Browser {
-    
+
+    static WebDriver driver;
+
+    private static String webdriverName = "Chrome";
+
+    public static void setWebdriver(){
+        if(webdriverName.equalsIgnoreCase("Chrome")){
+            driver = new ChromeDriver();
+        } else if (webdriverName.equalsIgnoreCase("Firefox")) {
+            driver = new FirefoxDriver();
+        }else{
+            driver = new EdgeDriver();
+        }
+    }
+
+    public static WebDriver getDriver(){
+        return driver;
+    }
 }
--- src/main/java/pages/Contact.java
+++ src/main/java/pages/Contact.java
@@ -1,4 +1,18 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Contact {
+    @FindBy(name = "First Name")
+    WebElement firstname;
+    @FindBy(name = "Last Name")
+    WebElement lastname;
+    @FindBy(name = "email")
+    WebElement email;
+    @FindBy(name = "message")
+    WebElement comment;
+    @FindBy(css = "[value=\"SUBMIT\"]")
+    WebElement submitButton;
+
 }
