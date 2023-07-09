ac19f374a88b6e0b04477afe07cfc31e48143255
Ishak1818 <sellamiishak18@gmail.com>
2023-07-09 16:40:41 -0400

    updating the web browser and added web elements

--- src/main/java/browser/Browser.java
+++ src/main/java/browser/Browser.java
@@ -2,8 +2,28 @@ package browser;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Browser {
+    private static String webDriverName = "Chrome";
+   private static WebDriver driver;
 
+    public static void setDriver(){
+        if (webDriverName.equalsIgnoreCase("chrome")){
+            driver = new ChromeDriver();
+
+        } else if (webDriverName.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver(); }
+        else {
+            driver = new EdgeDriver(); }
+
+
+
+
+    }
+
+     public static WebDriver getWebDriver(){
+        return driver;
+     }
 }
--- src/main/java/pages/Contact.java
+++ src/main/java/pages/Contact.java
@@ -1,4 +1,14 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Contact {
+
+    @FindBy(name = "first_name") private WebElement firstName;
+    @FindBy(name = "last_name") private WebElement lastName;
+    @FindBy(name = "email") private WebElement email;
+    @FindBy(name = "message") private WebElement comments;
+    @FindBy(css = "[value=\"RESET\"]") private WebElement resetButton;
+    @FindBy(css = "[value=\"SUBMIT\"]") private WebElement submitButton;
 }
